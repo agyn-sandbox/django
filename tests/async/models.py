b04522618902b1bca7d069503677aa6fd2ddfe70
Casey Brooks <casey.brooks@agyn.io>
2025-12-23 18:32:37 +0000

    fix(relations): add async related manager wrappers [skip ci]

--- tests/async/models.py
+++ tests/async/models.py
@@ -1,3 +1,5 @@
+from django.contrib.contenttypes.fields import GenericForeignKey, GenericRelation
+from django.contrib.contenttypes.models import ContentType
 from django.db import models
 from django.utils import timezone
 
@@ -9,3 +11,46 @@ class RelatedModel(models.Model):
 class SimpleModel(models.Model):
     field = models.IntegerField()
     created = models.DateTimeField(default=timezone.now)
+
+
+class AsyncAuthor(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class AsyncBook(models.Model):
+    author = models.ForeignKey(AsyncAuthor, models.CASCADE, related_name="books")
+    title = models.CharField(max_length=100)
+    pages = models.IntegerField(default=0)
+
+
+class AsyncGroup(models.Model):
+    name = models.CharField(max_length=100)
+    members = models.ManyToManyField(
+        "AsyncMember",
+        through="GroupMembership",
+        related_name="groups",
+    )
+
+
+class AsyncMember(models.Model):
+    name = models.CharField(max_length=100)
+    title = models.CharField(max_length=100, blank=True)
+
+
+class GroupMembership(models.Model):
+    group = models.ForeignKey(AsyncGroup, models.CASCADE)
+    member = models.ForeignKey(AsyncMember, models.CASCADE)
+    note = models.CharField(max_length=100, blank=True)
+
+
+class GenericNote(models.Model):
+    content_type = models.ForeignKey(ContentType, models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey()
+    message = models.CharField(max_length=100)
+    extra = models.CharField(max_length=100, blank=True)
+
+
+class GenericArticle(models.Model):
+    name = models.CharField(max_length=100)
+    notes = GenericRelation(GenericNote, related_query_name="articles")
