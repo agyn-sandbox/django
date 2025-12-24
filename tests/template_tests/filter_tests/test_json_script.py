656ddcf2d58bf1c757c021e8db50a73b6a8aeede
Casey Brooks <casey.brooks@agyn.io>
2025-12-24 12:50:41 +0000

    feat(utils): allow json_script without id

--- tests/template_tests/filter_tests/test_json_script.py
+++ tests/template_tests/filter_tests/test_json_script.py
@@ -17,3 +17,31 @@ class JsonScriptTests(SimpleTestCase):
             '{"a": "testing\\r\\njson \'string\\" \\u003Cb\\u003Eescaping\\u003C/b\\u003E"}'
             '</script>'
         )
+
+    @setup({'json-tag02': '{{ value|json_script }}'})
+    def test_without_id(self):
+        output = self.engine.render_to_string(
+            'json-tag02',
+            {'value': '&<>'}
+        )
+        self.assertEqual(
+            output,
+            (
+                '<script type="application/json">'
+                '"\\u0026\\u003C\\u003E"</script>'
+            )
+        )
+
+    @setup({'json-tag03': '{{ value|json_script:element_id }}'})
+    def test_none_id(self):
+        output = self.engine.render_to_string(
+            'json-tag03',
+            {'value': '&<>', 'element_id': None}
+        )
+        self.assertEqual(
+            output,
+            (
+                '<script type="application/json">'
+                '"\\u0026\\u003C\\u003E"</script>'
+            )
+        )
