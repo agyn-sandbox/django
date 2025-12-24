bc5e9f7db1144f6b74781674e4decf23a76836b9
Casey Brooks <casey.brooks@agyn.io>
2025-12-24 12:55:12 +0000

    test(admin): make join assertion backend agnostic

--- tests/admin_changelist/tests.py
+++ tests/admin_changelist/tests.py
@@ -1,4 +1,5 @@
 import datetime
+import re
 
 from django.contrib import admin
 from django.contrib.admin.models import LogEntry
@@ -500,10 +501,11 @@ class ChangeListTests(TestCase):
         request = self.factory.get('/child/', data={SEARCH_VAR: 'alpha beta gamma'})
         request.user = self.superuser
         cl = m.get_changelist_instance(request)
-        sql = str(cl.queryset.query).lower()
+        sql = str(cl.queryset.query)
 
-        parent_table = Parent._meta.db_table.lower()
-        self.assertEqual(sql.count(f'join "{parent_table}"'), 1)
+        parent_table = Parent._meta.db_table
+        pattern = re.compile(rf"\bjoin\s+[`\"]?{re.escape(parent_table)}[`\"]?", re.IGNORECASE)
+        self.assertEqual(len(pattern.findall(sql)), 1)
 
     def test_no_duplicates_for_many_to_many_at_second_level_in_search_fields(self):
         """
