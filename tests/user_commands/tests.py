1f187bcf1bfa8234662e45e1cee12020b7a3b6ee
Rowan Stein <rowan.stein@agyn.io>
2026-01-11 19:10:43 +0000

    tests(user_commands): restore unrecognized option error message expectations; adjust subparser invalid choice expectation quotes

--- tests/user_commands/tests.py
+++ tests/user_commands/tests.py
@@ -193,7 +193,7 @@ class CommandTests(SimpleTestCase):
             dance.Command.requires_migrations_checks = requires_migrations_checks
 
     def test_call_command_unrecognized_option(self):
-        msg = "Error: argument {foo}: invalid choice: 'test' (choose from 'foo')"
+        msg = (
             'Unknown option(s) for dance command: unrecognized. Valid options '
             'are: example, force_color, help, integer, no_color, opt_3, '
             'option3, pythonpath, settings, skip_checks, stderr, stdout, '
@@ -202,7 +202,7 @@ class CommandTests(SimpleTestCase):
         with self.assertRaisesMessage(TypeError, msg):
             management.call_command('dance', unrecognized=1)
 
-        msg = "Error: argument {foo}: invalid choice: 'test' (choose from 'foo')"
+        msg = (
             'Unknown option(s) for dance command: unrecognized, unrecognized2. '
             'Valid options are: example, force_color, help, integer, no_color, '
             'opt_3, option3, pythonpath, settings, skip_checks, stderr, '
