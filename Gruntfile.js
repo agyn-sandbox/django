dd78816953a0cdc5e608ffe37910088f07e8bd08
Casey Brooks <casey.brooks@agyn.io>
2026-01-13 19:56:12 +0000

    chore(ci): stabilize flake8 and puppeteer

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,10 +1,18 @@
 'use strict';
 
 const globalThreshold = 50; // Global code coverage threshold (as a percentage)
+const isCI = process.env.CI === 'true';
+const puppeteerExecutable = process.env.PUPPETEER_EXECUTABLE_PATH;
 
 module.exports = function(grunt) {
     grunt.initConfig({
         qunit: {
+            options: {
+                puppeteer: {
+                    args: isCI ? ['--no-sandbox', '--disable-setuid-sandbox'] : [],
+                    executablePath: puppeteerExecutable,
+                }
+            },
             all: ['js_tests/tests.html']
         }
     });
